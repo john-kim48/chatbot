0f909a7ad2ba31312b9631927b1e63cd18aba9bc
John <canadajohnkim@gmail.com>
2025-04-22 19:56:31 -0400

    changed start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0:$PORT app:app
+exec gunicorn --bind=0.0.0.0:$PORT app:app \
+  --workers 4 \
+  --capture-output \
+  --enable-stdio-inheritance \
+  --access-logfile - \
+  --error-logfile - \
+  --log-level debug
