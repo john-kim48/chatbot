cba6f634254cac20fa6bcf17df0ab0a1d3cc8356
John <canadajohnkim@gmail.com>
2025-04-22 11:34:32 -0400

    changed workflow

--- app.py
+++ app.py
@@ -6,20 +6,26 @@ from flask_cors import CORS
 import numpy as np
 import faiss
 from models import FaissIndexStore
+import time
 
 # Initialize Flask app and database
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 db.init_app(app)
+
+print("app starting")
 
 with app.app_context():
     db.create_all()
     index_data = FaissIndexStore.query.first()
     if index_data:
+        start = time.time()
         index_array = np.frombuffer(index_data.faiss_index, dtype=np.uint8)
         app.index = faiss.deserialize_index(index_array)
-        print("FAISS index loaded successfully.")
+        end = time.time()
+        print(f"FAISS index loaded in {end - start:.2f} seconds.")
     else:
         print("No FAISS index found in the database.")
         app.index = None
